ea9e306c3fca3683938dabb74d57b8fbcbc0cdb8
Claire Tsai <claire.tsai@onelab.tw>
2022-05-16 22:10:41 +0800

    fix: update uploadImage

--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const checkUpload = require('../service/uploadImage')
-
+const { isAuth } = require('../service/auth')
 const uploadImageControllers = require('../controllers/uploadImage')
 
-router.post('/', checkUpload, uploadImageControllers.uploadImage)
+router.post('/', isAuth, checkUpload, uploadImageControllers.uploadImage)
 
 module.exports = router
