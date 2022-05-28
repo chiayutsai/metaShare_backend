c39d5e281f3005d7c0c745ffd57aac767fc35f9a
Claire Tsai <claire.tsai@onelab.tw>
2022-05-28 22:02:26 +0800

    refactor: WebSocketService to class

--- service/webSocket.js
+++ service/webSocket.js
@@ -2,115 +2,155 @@ const SocketServer = require('ws').Server
 const jwt = require('jsonwebtoken')
 const Channel = require('../models/ChannelModel')
 
-const createWebsocket = (server) => {
-  const wss = new SocketServer({ server })
-  let conns = {}
-  wss.on('connection', (ws) => {
-    console.log('Client connected')
-    // 當收到client消息時
-    ws.on('message', async (data) => {
-      // 收回來是 Buffer 格式、需轉成字串
-      console.log(JSON.parse(data))
-      const { commandCode } = JSON.parse(data)
-      if (commandCode == '00') {
-        const { token } = JSON.parse(data)
-        try {
-          const decoded = await jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
-            if (err) {
-              throw err
-            } else {
-              return payload
-            }
-          })
-          conns[decoded.id] = ws
-          ws.send(
-            JSON.stringify({
-              commandCode: '01',
-              message: '連線成功',
-              online: Object.keys(conns),
-            })
-          )
+const CMD_CODE = {
+  // ACTION
+  WEB_SOCKET_LOGIN_REQUEST: '00',
+  WEB_SOCKET_LOGIN_RESPONSE: '01',
 
-          // 廣播上線通知
-          wss.clients.forEach((client) => {
-            client.send(
-              JSON.stringify({
-                commandCode: '30',
-                userId: decoded.id,
-              })
-            )
-          })
-        } catch (err) {
-          ws.send(
-            JSON.stringify({
-              commandCode: '01',
-              message: '連線失敗',
-            })
-          )
-        }
-      }
-      if (commandCode == '10') {
-        const { from, to, message } = JSON.parse(data)
-        await Channel.findOneAndUpdate(
-          { $and: [{ user: { $in: to } }, { user: { $in: from } }] },
-          { $push: { messages: { from, message } } },
-          { returnDocument: 'after' }
-        )
-        ws.send(
-          JSON.stringify({
-            commandCode: '11',
-            from,
-            to,
-            message,
-            createAt: Date.now(),
-          })
-        )
-        conns[to]?.send(
-          JSON.stringify({
-            commandCode: '20',
-            from,
-            to,
-            message,
-            createAt: Date.now(),
-          })
-        )
-      }
-      if (commandCode == '40') {
-        const { to, userId } = JSON.parse(data)
-        let channel = await Channel.findOne({ $and: [{ user: { $in: to } }, { user: { $in: userId } }] })
+  GET_CHANNEL_HISTORY_REQUEST: '40',
+  GET_CHANNEL_HISTORY_RESPONSE: '41',
 
-        if (!channel) {
-          channel = await Channel.create({ user: [to, userId] })
-        }
-        ws.send(
-          JSON.stringify({
-            commandCode: '41',
-            channel,
-            channelId: to,
-          })
-        )
-      }
-    })
-    // 當連線關閉
-    ws.on('close', () => {
-      const objArray = Object.entries(conns)
-      objArray.forEach((item) => {
-        if (item[1] === ws) {
-          delete conns[item[0]]
-
-          wss.clients.forEach((client) => {
-            client.send(
-              JSON.stringify({
-                commandCode: '31',
-                userId: item[0],
-              })
-            )
-          })
-        }
-      })
-      console.log('Close connected')
-    })
-  })
+  SEND_CHAT_MESSAGE_REQUEST: '10',
+  SEND_CHAT_MESSAGE_RESPONSE: '11',
+  // BC
+  CHAT_MESSAGE_NOTIFY: '20',
+  USER_LOGIN_NOTIFY: '30',
+  USER_LOGOUT_NOTIFY: '31',
 }
 
-module.exports = createWebsocket
+class WebSocketService {
+  #connections = {
+    // [userId]: ws
+  }
+
+  #MAPPER = null
+
+  constructor() {
+    this.#MAPPER = {
+      [CMD_CODE.WEB_SOCKET_LOGIN_REQUEST]: this.#handleWebSocketLoginRequest,
+      [CMD_CODE.SEND_CHAT_MESSAGE_REQUEST]: this.#handleSendChatMessageRequest,
+      [CMD_CODE.GET_CHANNEL_HISTORY_REQUEST]: this.#handleGetChannelHistoryRequest,
+    }
+  }
+
+  create = (server) => {
+    this.wss = new SocketServer({ server })
+
+    this.wss.on('connection', (ws) => {
+      console.log('Client connected')
+
+      // 當收到 client 消息時
+      ws.on('message', async (text) => {
+        this.#onMessage(ws, text)
+      })
+
+      // 當連線關閉
+      ws.on('close', () => {
+        this.#onClose(ws)
+      })
+    })
+  }
+
+  // message handler
+  #handleWebSocketLoginRequest = async (ws, data) => {
+    const { token } = data
+    try {
+      const decoded = await jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+        if (err) {
+          throw err
+        } else {
+          return payload
+        }
+      })
+
+      // 加到連線列表中
+      this.#connections[decoded.id] = ws
+
+      // 回應成功
+      this.#send(ws, CMD_CODE.WEB_SOCKET_LOGIN_RESPONSE, { message: '連線成功', online: Object.keys(this.#connections) })
+
+      // 廣播上線通知
+      this.broadcast(CMD_CODE.USER_LOGIN_NOTIFY, {
+        userId: decoded.id,
+      })
+    } catch (err) {
+      // 回應失敗
+      this.#send(ws, CMD_CODE.WEB_SOCKET_LOGIN_RESPONSE, { message: '連線失敗' })
+    }
+  }
+
+  #handleSendChatMessageRequest = async (ws, data) => {
+    const { from, to, message } = data
+    await Channel.findOneAndUpdate(
+      { $and: [{ user: { $in: to } }, { user: { $in: from } }] },
+      { $push: { messages: { from, message } } },
+      { returnDocument: 'after' }
+    )
+
+    // 回應成功
+    this.#send(ws, CMD_CODE.SEND_CHAT_MESSAGE_RESPONSE, { from, to, message, createAt: Date.now() })
+
+    // 傳訊息給對方
+    this.sendToUser(to, CMD_CODE.CHAT_MESSAGE_NOTIFY, { from, to, message, createAt: Date.now() })
+  }
+
+  #handleGetChannelHistoryRequest = async (ws, data) => {
+    const { to, userId } = data
+    let channel = await Channel.findOne({ $and: [{ user: { $in: to } }, { user: { $in: userId } }] })
+
+    if (!channel) {
+      channel = await Channel.create({ user: [to, userId] })
+    }
+
+    // 回應成功
+    this.#send(ws, CMD_CODE.GET_CHANNEL_HISTORY_RESPONSE, { channel, channelId: to })
+  }
+
+  // connection handler
+  #onMessage = (ws, text) => {
+    const data = JSON.parse(text)
+
+    console.log('message: ', data)
+
+    const { commandCode } = data
+
+    this.#MAPPER[commandCode]?.(ws, data)
+  }
+
+  #onClose = (ws) => {
+    console.log('Close connected')
+
+    // 重連線列表中移除, 並且廣播下線訊息
+    Object.entries(this.#connections).forEach(([userId, wsInstance]) => {
+      if (wsInstance === ws) {
+        delete this.#connections[userId]
+
+        this.broadcast(CMD_CODE.USER_LOGOUT_NOTIFY, {
+          userId,
+        })
+      }
+    })
+  }
+
+  #send = (client, code, data) => {
+    client?.send(
+      JSON.stringify({
+        commandCode: code,
+        ...data,
+      })
+    )
+  }
+
+  // 傳輸方法
+  sendToUser = (userId, code, data) => this.#send(this.#connections[userId], code, data)
+
+  broadcast = (code, data) => {
+    this.wss.clients.forEach((client) => {
+      this.#send(client, code, data)
+    })
+  }
+}
+
+const webSocketService = new WebSocketService()
+
+module.exports = webSocketService
